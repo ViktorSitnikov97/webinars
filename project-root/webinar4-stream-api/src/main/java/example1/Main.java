cb2d54e6be1821bb69eeacaeaddb15a0c2866b9c
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-07-19 20:49:18 +0300

    update Main (add examples)

--- project-root/webinar4-stream-api/src/main/java/example1/Main.java
+++ project-root/webinar4-stream-api/src/main/java/example1/Main.java
@@ -1,18 +1,87 @@
 package example1;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.function.DoubleToIntFunction;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         filterExample();
-        exampleGroupingBy();
+        groupingByExample();
         System.out.println(ExampleType.CORRECT);
         System.out.println(ExampleType.WRONG);
+
+        long count = Stream.of("1", "2", "3", "4", "5")
+                .map(Integer::parseInt)
+                .filter(i -> i > 2)
+                .count();
+        System.out.println(count);
+        flatMapExample();
+        advancedMapExample1();
+
+        List<Integer> examples = new ArrayList<>(List.of(1, 2, 3));
+        for (var item: examples) {
+            item =+ item + 10;
+        }
+        System.out.println(examples);
+
+        // Внутри метода forEach() содержится Consumer => возвращаемое значение void. Поэтому изменить
+        // через метод forEach() можно только те объекты, на которые есть ссылки:
+        examples.forEach(item -> item = item + 10);
+        System.out.println(examples);
+
+        List<Example> examplesNew = getExamples();
+        System.out.println(examplesNew);
+        examplesNew.forEach(ex -> ex.setExample("GG, WP"));
+        // Модифицировать коллекцию при прохождении ее элементов запрещено:
+        examplesNew.forEach(ex -> {
+            ex.setExample("GG, WP");
+            examplesNew.remove(ex);
+        });
+        System.out.println(examplesNew);
+         */
+
     }
 
-    public static void exampleGroupingBy() {
+    public static void advancedMapExample1() {
+        // обработать stream stream'ов и вернуть его же обратно
+        List<List<String>> examples = List.of(
+                List.of("1", "2", "3", "4"),
+                List.of("11", "22", "33", "44"),
+                List.of("", "222", "333", ""),
+                List.of("1111", "", "3333", "4444")
+        );
+        System.out.println(examples);
+        List<List<String>> examplesNew = examples.stream()
+                .map(list -> list.stream()
+                        .filter(l -> !l.isEmpty())
+                        .collect(Collectors.toList()))
+                .toList();
+        System.out.println(examplesNew);
+
+    }
+
+    public static void flatMapExample() {
+        List<List<String>> examples = List.of(
+                List.of("1", "2", "3", "4"),
+                List.of("11", "22", "33", "44"),
+                List.of("111", "222", "333", "444"),
+                List.of("1111", "2222", "3333", "4444")
+        );
+
+        List<Integer> result = examples.stream()
+                .flatMap(Collection::stream)
+                .map(Integer::parseInt)
+                .toList();
+        System.out.println(result);
+    }
+
+    public static void groupingByExample() {
         List<Example> examples = getExamples();
 
         Map<ExampleType, List<Example>> collect = examples.stream()
@@ -24,7 +93,7 @@ public class Main {
         List<Example> exampleList = getExamples();
         List<Example> filteredExampleList = exampleList.stream()
                 .filter(ex -> ex.getType() == ExampleType.CORRECT)
-                .collect(Collectors.toList()); // toList();
+                .collect(Collectors.toList()); // toList(); - 16 version of Java
         System.out.println(exampleList);
     }
 
