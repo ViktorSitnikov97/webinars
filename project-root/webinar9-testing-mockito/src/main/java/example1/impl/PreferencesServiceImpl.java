2bd7c31e9d0829d4eecbbb9b9918b742845f422d
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2025-08-23 02:28:02 +0300

    update PreferencesServiceImpl.java

--- project-root/webinar9-testing-mockito/src/main/java/example1/impl/PreferencesServiceImpl.java
+++ project-root/webinar9-testing-mockito/src/main/java/example1/impl/PreferencesServiceImpl.java
@@ -3,11 +3,34 @@ package example1.impl;
 import example1.PreferencesService;
 import example1.enums.Preference;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import static example1.enums.Preference.FOOTBALL;
+import static example1.enums.Preference.READING;
+import static example1.enums.Preference.WALKING;
+import static example1.enums.Preference.WATCHING_FILMS;
 
 public class PreferencesServiceImpl implements PreferencesService {
     @Override
-    Set<Preference> get(String userId) {
-        
+    public Set<Preference> get(String userId) {
+        return switch (userId) {
+            case "1" -> new HashSet<>(
+                    List.of(FOOTBALL, READING, WATCHING_FILMS, WALKING)
+            );
+            case "2" -> new HashSet<>(
+                    List.of(Preference.WATCHING_FILMS, Preference.WATCHING_FILMS)
+            );
+            case "3" -> new HashSet<>(
+                    List.of(WATCHING_FILMS, WALKING)
+            );
+            case "11" -> new HashSet<>(
+                    List.of(FOOTBALL, WALKING)
+            );
+            default -> new HashSet<>(
+                    List.of(WALKING)
+            );
+        };
     }
 }
